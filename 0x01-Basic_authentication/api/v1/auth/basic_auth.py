399cf39ac80812c46c8c8876fde5478259d83a9e
Uko Uwatt <uwattuko@gmail.com>
2024-08-08 06:51:57 +0100

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     '''Basic_Auth class'''
+    def extract_base64_authorization_header(
+        self, authorization_header: str) -> str:
+        '''extracts the authorization header'''
+        if not authorization_header:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if 'Basic ' not in authorization_header:
+            return None
+        return authorization_header[6:]
