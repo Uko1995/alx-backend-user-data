238fece74514784e69752ee14f5070502310b0f0
Uko Uwatt <uwattuko@gmail.com>
2024-08-09 06:01:25 +0100

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 ''' session auth class module '''
 from api.v1.auth.auth import Auth
+from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
     '''session auth class'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''creates a session ID'''
+        if not isinstance(user_id, str) or user_id is None:
+            return None
+
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
