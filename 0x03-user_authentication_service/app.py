12d3f4bd3e648fedb5d00df3b5b60bcd9e3e445b
Uko Uwatt <uwattuko@gmail.com>
2024-08-15 06:11:09 +0100

    task 6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 '''basic flask app'''
 from flask import Flask, jsonify
-from auth import Auth
-from db import DB
 
 app = Flask(__name__)
-db = DB()
-auth = Auth()
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
     '''basic index route'''
-    return jsonify("message": "Bienvenue")
+    return jsonify({"message": "Bienvenue"})
 
 
-if __name__ = "__main__":
-    app.run(host='0.0.0..0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
