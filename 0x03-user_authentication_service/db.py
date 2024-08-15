1bb58937c9350698835d05309e8b51b281559265
Uko Uwatt <uwattuko@gmail.com>
2024-08-15 04:26:53 +0100

    corrected task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
         return new_user
 
     def find_user_by(self, **kwargs: str) -> User:
-        '''returns the first row found on the users table'''
+        '''returns the first row found on the users table
         session = self._session
         user = session.query(User).filter_by(**kwargs).all()
         valid = User.__table__.columns.keys()
@@ -51,4 +51,8 @@ class DB:
 
         if user == []:
             raise NoResultFound('Not found')
-        return user[0]
+        return user[0]'''
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
