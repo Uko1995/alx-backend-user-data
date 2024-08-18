f1537966b9c224a1237bcb07844feeac63edd40a
Uko Uwatt <uwattuko@gmail.com>
2024-08-18 23:15:33 +0100

    task correction

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,8 +11,8 @@ Base = declarative_base()
 class User(Base):
     '''user class'''
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer(), primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
