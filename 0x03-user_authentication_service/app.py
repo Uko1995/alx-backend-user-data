2d0ef354d0fd021175448da47570f8ffab21e68d
Uko Uwatt <uwattuko@gmail.com>
2024-08-19 00:45:13 +0100

    task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,5 +63,17 @@ def profile() -> str:
         return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password() -> str:
+    '''resets password'''
+    email = request.get('email')
+    user = self._db.find_user_by(email=email)
+    if not user:
+        abort(403)
+    else:
+        token = AUTH.get_reset_token(email)
+        return jsonify({"email": user.email, "reset_token": token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
