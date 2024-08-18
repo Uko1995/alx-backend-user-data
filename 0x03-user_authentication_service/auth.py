ffec990f3409cd95eb7ae2a2af36bf461337c091
Uko Uwatt <uwattuko@gmail.com>
2024-08-18 23:33:05 +0100

    task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,10 +55,12 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> User:
         '''takes session_id and returns User'''
-        if session_id == None:
+        if session_id is None:
             return None
         try:
             user = self_db.find_user_by(session_id=session_id)
+            if not user:
+                return None
             return user
-        except NoResultFound:
+        except NoResultFound() or AttributeError:
             return None
